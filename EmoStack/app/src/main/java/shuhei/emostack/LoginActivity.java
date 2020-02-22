1b4132140f1956663cc580acedc87dd01f7fc55d
K2sei <k2sei.19970919@gmail.com>
2020-02-22 15:06:20 -0800

    dev login activity

--- EmoStack/app/src/main/java/shuhei/emostack/LoginActivity.java
+++ EmoStack/app/src/main/java/shuhei/emostack/LoginActivity.java
@@ -1,14 +1,126 @@
 package shuhei.emostack;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+
+    Button sign_up;
+    Button log_in;
+    EditText input_email;
+    EditText input_password;
+    String mEmail;
+    String mPassword;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        setTitle("LOG IN");
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        sign_up = (Button)findViewById(R.id.sign_in_button);
+        log_in = (Button)findViewById(R.id.log_in_button);
+        input_email = (EditText)findViewById(R.id.input_email);
+        input_password = (EditText)findViewById(R.id.input_password);
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmail = input_email.getText().toString();
+                mPassword = input_password.getText().toString();
+
+                mEmail = mEmail.trim();
+                mPassword = mPassword.trim();
+
+                if(mEmail.isEmpty() || mPassword.isEmpty()){
+                    AlertDialog.Builder builder;
+                    builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage("Please enter email and password")
+                            .setTitle("Error")
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else{
+                    mFirebaseAuth.createUserWithEmailAndPassword(mEmail,mPassword)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }else{
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle("Error")
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mEmail = input_email.getText().toString();
+                mPassword = input_password.getText().toString();
+
+                if(mEmail.isEmpty() || mPassword.isEmpty()){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                    builder.setMessage("Please make sure you enter email address and password")
+                            .setTitle("Error")
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }else{
+                    mFirebaseAuth.signInWithEmailAndPassword(mEmail,mPassword)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(intent);
+                                    }else{
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                                        builder.setMessage(task.getException().getMessage())
+                                                .setTitle("Error")
+                                                .setPositiveButton(android.R.string.ok, null);
+                                        AlertDialog dialog = builder.create();
+                                        dialog.show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return false;
     }
 }
